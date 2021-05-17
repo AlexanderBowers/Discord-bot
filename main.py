2da003169ad701414530fd05c419146245cf3446
AlexanderBowers <7319abowers@gmail.com>
2021-05-17 16:38:13 -0700

    now checks for sad words

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+starter_encouragements = ["Cheer up!", "Hang in there.", "You are a good person!"]
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -19,9 +24,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
+    msg = message.content
     
-    if message.content.startswith('$inspire'):
+    if msg.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
 
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
+        
 client.run(os.getenv('TOKEN'))
